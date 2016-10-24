6d04dbb062a328a66a81f24163f3ecedea79da13
Ben Kline <ben.kline@gmail.com>
2016-10-24 16:04:26 -0600

    add playSong function, comment all functions and variables in SongPlayer.js

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,10 +1,16 @@
 (function() {
     function SongPlayer() {
+
+/**
+* @desc  a variable set to an empty object so that the service returning this object sets its properties and methods public to the rest of the application.
+*/
         var SongPlayer = {};
+/**
+* @desc sets variable to null to refresh app for each instance
+*/
         var currentSong = null;
 /**
 * @desc Buzz object audio file
-* @type {Object}
 */
         var currentBuzzObject = null;
 /**
@@ -26,24 +32,44 @@
           currentSong = song;
         };
 
+/**
+* @function playSong
+* @desc sets currentBuzzObject to play and song.playing to true
+* @param {Object} song
+*/
+        var playSong = function(song){
+          currentBuzzObject.play();
+          song.playing = true;
+        }
+
+/**
+* @function SongPlayer.play
+* @desc makes currentBuzzObject publicly available, checks to see if it selected song is already selected - if not, then sets and plays - if selected is set as seleted but is paused, then plays it
+* @param {Object} song
+*/
         SongPlayer.play = function(song) {
           if (currentSong !== song) {
             setSong(song);
-            currentBuzzObject.play();
-            song.playing = true;
+            playSong(song);
 
           } else if (currentSong === song) {
               if (currentBuzzObject.isPaused()) {
-                  currentBuzzObject.play();
+                  playSong(song);
               }
           }
         };
 
+/**
+* @function SongPlayer.pause
+* @desc makes currentBuzzObject publicly available, sets currentBuzzObject to paused and song.playing to false
+* @param {Object} song
+*/
         SongPlayer.pause = function(song) {
           currentBuzzObject.pause();
           song.playing = false;
         };
-        return SongPlayer;
+
+      return SongPlayer;
     }
 
 
